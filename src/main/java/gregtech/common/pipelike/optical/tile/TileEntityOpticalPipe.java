0c697b9c7c86207b7a870b7e6957227415148dd7
Tech22 <37029404+TechLord22@users.noreply.github.com>
2023-06-19 15:55:50 -0400

    cleanup pipe tile

--- src/main/java/gregtech/common/pipelike/optical/tile/TileEntityOpticalPipe.java
+++ src/main/java/gregtech/common/pipelike/optical/tile/TileEntityOpticalPipe.java
@@ -5,7 +5,6 @@ import gregtech.api.capability.IDataAccessHatch;
 import gregtech.api.pipenet.tile.IPipeTile;
 import gregtech.api.pipenet.tile.TileEntityPipeBase;
 import gregtech.api.recipes.Recipe;
-import gregtech.api.util.FacingPos;
 import gregtech.common.pipelike.optical.OpticalPipeProperties;
 import gregtech.common.pipelike.optical.OpticalPipeType;
 import gregtech.common.pipelike.optical.net.OpticalNetHandler;
@@ -19,17 +18,14 @@ import javax.annotation.Nullable;
 import java.lang.ref.WeakReference;
 import java.util.Collection;
 import java.util.EnumMap;
-import java.util.HashMap;
-import java.util.Map;
 
 public class TileEntityOpticalPipe extends TileEntityPipeBase<OpticalPipeType, OpticalPipeProperties> {
 
     private final EnumMap<EnumFacing, OpticalNetHandler> handlers = new EnumMap<>(EnumFacing.class);
-    private final Map<FacingPos, Integer> transferred = new HashMap<>();
-    private OpticalNetHandler defaultHandler;
     // the OpticalNetHandler can only be created on the server, so we have an empty placeholder for the client
     private final IDataAccessHatch clientCapability = new DefaultDataHandler();
     private WeakReference<OpticalPipeNet> currentPipeNet = new WeakReference<>(null);
+    private OpticalNetHandler defaultHandler;
 
     @Override
     public Class<OpticalPipeType> getPipeTypeClass() {
@@ -83,8 +79,7 @@ public class TileEntityOpticalPipe extends TileEntityPipeBase<OpticalPipeType, O
         if (world == null || world.isRemote)
             return null;
         OpticalPipeNet currentPipeNet = this.currentPipeNet.get();
-        if (currentPipeNet != null && currentPipeNet.isValid() &&
-                currentPipeNet.containsNode(getPipePos()))
+        if (currentPipeNet != null && currentPipeNet.isValid() && currentPipeNet.containsNode(getPipePos()))
             return currentPipeNet; //if current net is valid and does contain position, return it
         WorldOpticalPipeNet worldNet = (WorldOpticalPipeNet) getPipeBlock().getWorldPipeNet(getPipeWorld());
         currentPipeNet = worldNet.getNetFromPos(getPipePos());
@@ -92,14 +87,6 @@ public class TileEntityOpticalPipe extends TileEntityPipeBase<OpticalPipeType, O
             this.currentPipeNet = new WeakReference<>(currentPipeNet);
         }
         return currentPipeNet;
-    }
-
-    public void resetTransferred() {
-        transferred.clear();
-    }
-
-    public Map<FacingPos, Integer> getTransferred() {
-        return transferred;
     }
 
     @Override
